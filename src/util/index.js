6bfff92410c740288fc804103c59a23ca288b7c4
easylogic <cyberuls@gmail.com>
2021-06-08 00:12:00 +0900

    remove image filter

--- src/util/index.js
+++ src/util/index.js
@@ -1,15 +1,9 @@
 import Color from './Color'
 import HueColor from './HueColor'
 import ColorNames from './ColorNames'
-import ImageFilter from './ImageFilter'
-import Canvas from './Canvas'
-import ImageLoader from './ImageLoader'
 
 export default {
-    Color, 
+    Color,
     HueColor,
     ColorNames,
-    ImageFilter,
-    Canvas, 
-    ImageLoader
 }
